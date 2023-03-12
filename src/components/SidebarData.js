0a03d537b7fd133f2276786ca1492cfc7e9e0a54
Anton Bondar <direx187@gmail.com>
2023-03-12 21:44:01 +0200

    first commit

---
+++ src/components/SidebarData.js
@@ -0,0 +1,32 @@
+import React from "react";
+import * as AiIcons from "react-icons/ai";
+import * as FaIcons from "react-icons/fa";
+import * as IoIcons from "react-icons/io";
+
+export const SidebarData = [
+    {
+        title: "Home",
+        path: "/",
+        icon: <AiIcons.AiFillHome/>,
+        cName: 'nav-text'
+    },
+    {
+        title: "Prosucts",
+        path: "/products",
+        icon: <AiIcons.AiOutlineAntDesign/>,
+        cName: 'nav-text'
+    },
+    {
+        title: "News",
+        path: "/news",
+        icon: <FaIcons.FaNewspaper/>,
+        cName: 'nav-text'
+    },
+    {
+        title: "Contacts",
+        path: "/contacts",
+        icon: <AiIcons.AiOutlineContacts/>,
+        cName: 'nav-text'
+    },
+]
+
